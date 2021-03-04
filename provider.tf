a6a36bad5ff620c19fa8bf36576db43cf4c5b19b
KirillSSS <63448843+KirillSSS@users.noreply.github.com>
2021-03-04 16:20:29 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    access_key = "AKIAIL2YJZGU5XYUR6EQ"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
     region = "eu-central-1"
 }
